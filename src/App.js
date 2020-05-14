2df6e7ac9e176965687beb50916b27035ca3bcbd
Elsa Filippidou <elsa.filippidou@gmail.com>
2020-05-14 13:24:38 +0200

    basic data visualisation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./styles/App.scss";
 import axios from "axios";
+import GraphContainer from "./components/GraphContainer";
 
-function App() {
+export default function App() {
   const [rawData, setRawData] = useState(null);
   const [dataByCategory, setDataByCategory] = useState(null);
   const [fundingByCategory, setFundingByCategory] = useState(null);
   const [roundsByCategory, setRoundsByCategory] = useState(null);
+  const [aggregatedDataByCategory, setAggregatedDataByCategory] = useState(
+    null
+  );
+  const [selectedDataType, setSelectedDataType] = useState("volumes");
 
   useEffect(() => {
     fetchData();
@@ -46,6 +51,7 @@ function App() {
 
     let categoriesFunding = {};
     let categoriesRounds = {};
+    let aggregatedData = [];
     for (let category in dataByCategory) {
       let fundingTotal = dataByCategory[category].reduce((acc, obj) => {
         return acc + obj.fundingAmount;
@@ -54,12 +60,40 @@ function App() {
 
       let rounds = dataByCategory[category].length;
       categoriesRounds[category] = rounds;
+
+      aggregatedData.push({
+        category: category,
+        funding: fundingTotal,
+        rounds: rounds,
+      });
     }
     setFundingByCategory(categoriesFunding);
     setRoundsByCategory(categoriesRounds);
+    setAggregatedDataByCategory(aggregatedData);
   }, [dataByCategory]);
 
-  return <div className="App"></div>;
-}
+  return (
+    <div className="App">
+      <h1>Funding By Industry - Analytics</h1>
+      <label htmlFor="dataType">Data: </label>
 
-export default App;
+      <select
+        id="dataType"
+        onChange={(event) => {
+          console.log(event.target.value);
+          setSelectedDataType(event.target.value);
+        }}
+      >
+        <option value="volumes">Funding amount</option>
+        <option value="rounds">Funding rounds</option>
+      </select>
+      <GraphContainer
+        selectedDataType={selectedDataType}
+        data={{
+          dataByCategory: dataByCategory,
+          aggregatedDataByCategory: aggregatedDataByCategory,
+        }}
+      ></GraphContainer>
+    </div>
+  );
+}
